84b0626a16ad9725b4b30aa3500d70e016128b6b
BigCoderDominykas <146240089+BigCoderDominykas@users.noreply.github.com>
2023-11-09 21:25:26 +0200

    Added a delay to the fade out animation

--- Assets/Scripts/FadeOut.cs
+++ Assets/Scripts/FadeOut.cs
@@ -5,19 +5,25 @@ using UnityEngine;
 public class FadeOut : MonoBehaviour
 {
     public float duration;
+    public float fadeDelay;
 
     void Start()
     {
         duration += Random.Range(-1f, 1f);
-        Destroy(gameObject, duration);
+        Destroy(gameObject, duration + fadeDelay);
     }
 
     void Update()
     {
-        Color color = this.GetComponent<Renderer>().material.color;
-        float fadeAmount = color.a - (Time.deltaTime / duration);
+        fadeDelay -= Time.deltaTime;
 
-        color = new Color(color.r, color.g, color.b, fadeAmount);
-        this.GetComponent<Renderer>().material.color = color;
+        if(fadeDelay <= 0f)
+        {
+            Color color = this.GetComponent<Renderer>().material.color;
+            float fadeAmount = color.a - (Time.deltaTime / duration);
+
+            color = new Color(color.r, color.g, color.b, fadeAmount);
+            this.GetComponent<Renderer>().material.color = color;
+        }
     }
 }
